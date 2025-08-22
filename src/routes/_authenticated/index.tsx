4a53bf9a26ffe978f2e08c6a339c9a4294656801
SvenFE <shensven2016@gmail.com>
2025-08-22 21:36:17 +0800

    chore: 根路由定向到 `/vector-stores`

--- src/routes/_authenticated/index.tsx
+++ src/routes/_authenticated/index.tsx
@@ -1,6 +1,13 @@
-import { createFileRoute } from '@tanstack/react-router'
-import { Dashboard } from '@/features/dashboard'
+import { createFileRoute, redirect } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/_authenticated/')({
-  component: Dashboard,
+  beforeLoad: () => {
+    throw redirect({ to: '/vector-stores' })
+  },
 })
+
+// import { Dashboard } from '@/features/dashboard'
+
+// export const Route = createFileRoute('/_authenticated/')({
+//   component: Dashboard,
+// })
